bab7047b7f098d41a829c4df8e46120e1c6e35ca
DuyBua <mondorea42@gmail.com>
2026-04-27 21:51:15 +0700

    Init project

---
+++ src/main/java/duy/project/yoot_management/YootManagementApplication.java
@@ -0,0 +1,13 @@
+package duy.project.yoot_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YootManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YootManagementApplication.class, args);
+	}
+
+}
